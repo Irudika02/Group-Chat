52b5838b42508f3c0e15033ed48b2c0f4a1cd159
Irudika02 <iriyaaa12345@gmail.com>
2022-08-14 21:41:22 +0530

    Create group chat box

---
+++ src/server/ServerOpen.java
@@ -0,0 +1,12 @@
+package server;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ServerOpen  {
+
+    public static void main(String[] args) {
+        new Server();
+    }
+}
+
